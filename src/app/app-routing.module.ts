f7be59085769e26505001e9a490f2192ebe1fc5c
bhaveet007 <bhaveetseekin14@gmail.com>
2019-04-26 23:37:23 +0530

    This is the first commit for the Angular app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+//import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from 
+'./components/home/home.component';
+
+import { UsersComponent } from 
+'./components/users/users.component';
+
+import { PostsComponent } from 
+'./components/posts/posts.component';
+
+import { PostComponent } from
+'./components/post/post.component';
+
+import { NotFoundComponent } from
+'./components/not-found/not-found.component';
+
+const routes: Routes =  [
+  { path: '', component: HomeComponent},
+  { path: 'users', component: UsersComponent}, 
+  { path: 'posts', component: PostsComponent},
+  { path: 'post/:id', component: PostComponent},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+  exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(routes)
+  ]
 })
 export class AppRoutingModule { }
